8729cb7597a89c5eb4e004e90e7ae12e4c44b8b2
Yash <ymalegao@ucsc.edu>
2025-05-07 11:39:18 -0700

    script updated

--- start_client.sh
+++ start_client.sh
@@ -4,13 +4,15 @@ SERVER_IP="34.152.55.206"
 PORT="443"
 TUN_IF="utun9"
 
-# 1. Run the VPN client binary
+# 1. Run VPN client in background
 sudo ./bazel-bin/vpn_client $SERVER_IP $PORT $TUN_IF &
 
-# 2. Configure TUN interface
+# 2. Wait for utun9 to be created
+echo "Waiting for $TUN_IF..."
+while ! ifconfig $TUN_IF >/dev/null 2>&1; do sleep 0.2; done
+echo "$TUN_IF is up"
+
+# 3. Assign IP and set default route
 sudo ifconfig $TUN_IF inet 10.8.0.2 10.8.0.1 netmask 255.255.255.255 up
-
-# 3. Set default route through VPN
-sudo route delete default
+sudo route delete default || true
 sudo route add default 10.8.0.1
-
--- start_server.sh
+++ start_server.sh
@@ -9,14 +9,16 @@ KEY="server.key"
 # 1. Enable IP forwarding
 sudo sysctl -w net.ipv4.ip_forward=1
 
-# 2. Bring up TUN interface with IP
-sudo ip link set $TUN_IF up || sudo ip tuntap add dev $TUN_IF mode tun
+# 2. Run VPN server in background
+sudo ./bazel-bin/vpn_server $PORT $TUN_IF "$PSK" $CERT $KEY &
+
+# 3. Wait for tun0 to be created
+echo "Waiting for $TUN_IF..."
+while ! ip link show $TUN_IF &>/dev/null; do sleep 0.2; done
+echo "$TUN_IF is up"
+
+# 4. Assign IP and NAT
 sudo ip addr flush dev $TUN_IF
 sudo ip addr add 10.8.0.1/24 dev $TUN_IF
-
-# 3. Setup NAT using iptables (replace eth0 if needed)
+sudo ip link set $TUN_IF up
 sudo iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
-
-# 4. Run VPN server
-sudo ./bazel-bin/vpn_server $PORT $TUN_IF "$PSK" $CERT $KEY
-
